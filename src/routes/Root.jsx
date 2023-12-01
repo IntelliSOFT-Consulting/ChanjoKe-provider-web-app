1f0539e46fbe15b418c3c9620f598f2f48c29ef5
Onejohi <tonnie254@live.com>
2023-12-01 15:09:12 +0300

    - create and configure router

---
+++ src/routes/Root.jsx
@@ -0,0 +1,14 @@
+export default function Root() {
+  return (
+    <div className="bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div className="mx-auto max-w-2xl text-center">
+        <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          Root ChanjoKE Web
+        </h2>
+        <p className="mt-6 text-lg leading-8 text-gray-600">
+          Ministry of Health | Kenya
+        </p>
+      </div>
+    </div>
+  )
+}
