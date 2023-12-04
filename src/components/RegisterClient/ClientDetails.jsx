f5523adacfd64dcdfaa3303e65aeb93737a3bafe
Onejohi <tonnie254@live.com>
2023-12-04 21:29:29 +0300

    - add basic component for client details - create admin management page - create AEFI Occurences and Defaulters List pages

---
+++ src/components/RegisterClient/ClientDetails.jsx
@@ -0,0 +1,7 @@
+export default function ClientDetails() {
+  return (
+    <>
+      Client Details Form
+    </>
+  )
+}
---
+++ src/pages/AdminManagement.jsx
@@ -0,0 +1,14 @@
+export default function AdminManagement() {
+  return (
+    <div className="bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div className="mx-auto max-w-2xl text-center">
+        <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          Admin Management
+        </h2>
+        <p className="mt-6 text-lg leading-8 text-gray-600">
+          Search user and update or archive their account
+        </p>
+      </div>
+    </div>
+  );
+}
