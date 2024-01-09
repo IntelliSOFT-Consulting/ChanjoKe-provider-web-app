58557a22cb5ee760c071f2e2859689a68de15d35
Onejohi <tonnie254@live.com>
2024-01-09 16:24:31 +0300

    - add search ui

--- src/pages/SearchClient.jsx
+++ src/pages/SearchClient.jsx
@@ -1,13 +1,31 @@
+import SearchTable from "../common/tables/SearchTable"
+import TextInput from "../common/forms/TextInput"
+
 export default function SearchClient() {
   return (
-    <div className="bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <div className="mx-auto max-w-2xl text-center">
-        <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-          Search Client Page
-        </h2>
-        <p className="mt-6 text-lg leading-8 text-gray-600">
-          Enter client name/id and search for client
-        </p>
+    <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow mt-5">
+      <div className="px-4 text-2xl font-semibold py-5 sm:px-6">
+        Search
+      </div>
+      <div className="px-4 py-5 sm:p-6">
+        <div className="grid grid-cols-5 gap-4 mx-10">
+          <div className="col-span-4">
+            <TextInput
+              inputType="text"
+              inputName="searchInput"
+              inputId="searchInput"
+              label="Enter Client Name/ID"
+              inputPlaceholder="Enter Client Name/ID"/>
+          </div>
+          <div>
+            <button
+              className="ml-4 mt-8 flex-shrink-0 rounded-full w-full bg-[#163C94] border border-[#163C94] outline outline-[#163C94] px-5 py-2 text-sm font-semibold text-white shadow-sm hover:bg-[#163C94] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#163C94]">
+              Search
+            </button>
+          </div>
+        </div>
+
+        <SearchTable />
       </div>
     </div>
   );
