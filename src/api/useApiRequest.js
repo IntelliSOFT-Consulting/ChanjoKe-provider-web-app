d8f2e4ded34fcddcd2e7810836c8cafd967e2d42
nyatindopatrick <nyatindopatrick@gmail.com>
2024-03-12 15:39:22 +0300

    add facilities creation and listing

---
+++ src/api/useApiRequest.js
@@ -0,0 +1,69 @@
+import axios from 'axios'
+import { message } from 'antd'
+
+const server = axios.create({
+  baseURL: 'https://chanjoke.intellisoftkenya.com/hapi/fhir',
+})
+
+server.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  (error) => {
+    if (error.response) {
+      message.error(error.response.data.message)
+    } else {
+      message.error('Network Error')
+    }
+    return Promise.reject(error)
+  }
+)
+
+export const useApiRequest = () => {
+  // abort controller
+  const abortController = new AbortController()
+
+  const get = async (url) => {
+    try {
+      const response = await server.get(url, { signal: abortController.signal })
+      return response.data
+    } catch (error) {
+      console.log('Error', error)
+    }
+  }
+
+  const post = async (url, data) => {
+    try {
+      const response = await server.post(url, data, {
+        signal: abortController.signal,
+      })
+      return response.data
+    } catch (error) {
+      console.log('Error', error)
+    }
+  }
+
+  const put = async (url, data) => {
+    try {
+      const response = await server.put(url, data, {
+        signal: abortController.signal,
+      })
+      return response.data
+    } catch (error) {
+      console.log('Error', error)
+    }
+  }
+
+  const remove = async (url) => {
+    try {
+      const response = await server.delete(url, {
+        signal: abortController.signal,
+      })
+      return response.data
+    } catch (error) {
+      console.log('Error', error)
+    }
+  }
+
+  return { get, post, put, remove }
+}
--- src/index.js
+++ src/index.js
@@ -1,60 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
 import routes from './routes/index'
-import {RouterProvider} from 'react-router-dom'
-import reportWebVitals from './reportWebVitals';
-import {ConfigProvider} from "antd";
+import { RouterProvider } from 'react-router-dom'
+import reportWebVitals from './reportWebVitals'
+import { ConfigProvider } from 'antd'
 
 const defaultData = {
-    borderRadius: 6,
+  borderRadius: 6,
+  colorPrimary: '#163C94',
+  Button: {
     colorPrimary: '#163C94',
-    Button: {
-        colorPrimary: '#163C94',
-    },
-    Input: {
-        colorPrimary: '#163C94',
-    },
-};
+  },
+  Input: {
+    colorPrimary: '#163C94',
+  },
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <ConfigProvider
-            theme={{
-                token: {
-                    colorPrimary: defaultData.colorPrimary,
-                    borderRadius: defaultData.borderRadius,
-                },
-                components: {
-                    Button: {
-                        colorPrimary: defaultData.Button?.colorPrimary,
-                        algorithm: defaultData.Button?.algorithm,
-                    },
-                    Input: {
-                        colorPrimary: defaultData.Input?.colorPrimary,
-                        algorithm: defaultData.Input?.algorithm,
-                    },
-                    Select: {
-                        colorPrimary: defaultData.Select?.colorPrimary,
-                        algorithm: defaultData.Select?.algorithm,
-                    },
-                    InputNumber: {
-                        colorPrimary: defaultData.InputNumber?.colorPrimary,
-                        algorithm: defaultData.InputNumber?.algorithm,
-                    },
-                    DatePicker: {
-                        colorPrimary: defaultData.DatePicker?.colorPrimary,
-                        algorithm: defaultData.DatePicker?.algorithm,
-                    },
-                },
-            }}
-        >
-            <RouterProvider router={routes}/>
-        </ConfigProvider>
-    </React.StrictMode>
-);
+  <React.StrictMode>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: defaultData.colorPrimary,
+          borderRadius: defaultData.borderRadius,
+        },
+        components: {
+          Button: {
+            colorPrimary: defaultData.Button?.colorPrimary,
+            algorithm: defaultData.Button?.algorithm,
+          },
+          Input: {
+            colorPrimary: defaultData.Input?.colorPrimary,
+            algorithm: defaultData.Input?.algorithm,
+          },
+          Select: {
+            colorPrimary: defaultData.Select?.colorPrimary,
+            algorithm: defaultData.Select?.algorithm,
+          },
+          InputNumber: {
+            colorPrimary: defaultData.InputNumber?.colorPrimary,
+            algorithm: defaultData.InputNumber?.algorithm,
+          },
+          DatePicker: {
+            colorPrimary: defaultData.DatePicker?.colorPrimary,
+            algorithm: defaultData.DatePicker?.algorithm,
+          },
+        },
+      }}
+    >
+      <RouterProvider router={routes} />
+    </ConfigProvider>
+  </React.StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
