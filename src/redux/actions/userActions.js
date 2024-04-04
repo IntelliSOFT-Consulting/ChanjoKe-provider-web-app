4150566a98a80fa97dd0716b2b13def3a6261886
nyatindopatrick <nyatindopatrick@gmail.com>
2024-04-04 12:04:11 +0300

    Refactor auth and add user information

---
+++ src/redux/actions/userActions.js
@@ -0,0 +1,82 @@
+import axios from 'axios'
+import {
+  GET_USER_REQUEST,
+  GET_USER_SUCCESS,
+  GET_USER_FAILURE,
+  LOGOUT,
+} from '../constants/userConstants'
+import { message } from 'antd'
+
+const token = localStorage.getItem('authorization')
+  ? JSON.parse(localStorage.getItem('authorization')).token
+  : null
+
+const server = axios.create({
+  baseURL: 'https://chanjoke.intellisoftkenya.com',
+  headers: {
+    'Cache-Control': 'no-cache',
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${token?.access_token}`,
+  },
+})
+
+server.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  async (error) => {
+    const originalRequest = error.config
+    if (error.response.status === 401 && !originalRequest._retry) {
+      // find the request endpoint that caused the 401
+      const endpoint = originalRequest.url
+      if (endpoint.includes('login')) {
+        return Promise.reject(error)
+      }
+
+      originalRequest._retry = true
+      const refreshToken = token?.refresh_token
+      const response = await axios.post(
+        'https://chanjoke.intellisoftkenya.com/auth/token',
+        {
+          grant_type: 'refresh_token',
+          refresh_token: refreshToken,
+        }
+      )
+      localStorage.setItem('authorization', JSON.stringify(response.data))
+      server.defaults.headers.common['Authorization'] =
+        `Bearer ${response.data.access_token}`
+      return server(originalRequest)
+    }
+    return Promise.reject(error)
+  }
+)
+
+export const login = (values) => async (dispatch) => {
+  dispatch({ type: GET_USER_REQUEST })
+  try {
+    const { data: auth } = await server.post('/auth/provider/login', values)
+    if (auth) {
+      const response = await server.get(`/auth/provider/me`, {
+        headers: {
+          Authorization: `Bearer ${auth.access_token}`,
+        },
+      })
+
+      const user = { ...response.data?.user, ...auth }
+      dispatch({
+        type: GET_USER_SUCCESS,
+        payload: user,
+      })
+
+      JSON.stringify(localStorage.setItem('practitioner', JSON.stringify(user)))
+    }
+  } catch (error) {
+    dispatch({ type: GET_USER_FAILURE, payload: error.response.data })
+    message.error(error.response?.data?.error)
+  }
+}
+
+export const logout = () => async (dispatch) => {
+  localStorage.removeItem('practitioner')
+  dispatch({ type: LOGOUT, payload: {} })
+}
--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,27 +1,35 @@
-import { Outlet } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 import Sidenav from '../components/Sidenav'
-import { AuthProvider } from '../AuthContext'
 import { SharedStateProvider } from '../shared/sharedState'
+import { useSelector } from 'react-redux'
 
 export default function Root() {
+  const { user } = useSelector((state) => state.userInfo)
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!user) {
+      navigate('/auth')
+    }
+  }, [user, navigate])
 
   return (
-    <AuthProvider>
-      <SharedStateProvider>
+    <SharedStateProvider>
+      {user && (
         <div>
           <Sidenav />
-
           <main className="py-10 lg:pl-72">
             <div className="px-4 sm:px-6 lg:px-8">
-
               <Navbar />
 
               <Outlet />
             </div>
           </main>
         </div>
-      </SharedStateProvider>
-    </AuthProvider>
+      )}
+    </SharedStateProvider>
   )
 }
