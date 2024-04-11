3f095157546583ded7bc29615ea977c35fa5ef4e
nyatindopatrick <nyatindopatrick@gmail.com>
2024-04-11 06:57:24 +0300

    fix not administered forms

--- src/components/AdministerVaccines/NotAdministered.jsx
+++ src/components/AdministerVaccines/NotAdministered.jsx
@@ -1,51 +1,49 @@
-import TextInput from "../../common/forms/TextInput"
-import SelectMenu from "../../common/forms/SelectMenu"
-import ConfirmDialog from "../../common/dialog/ConfirmDialog"
-import FormState from "../../utils/formState"
-import { useSharedState } from "../../shared/sharedState"
-import { useEffect, useState } from "react"
+import TextInput from '../../common/forms/TextInput'
+import moment from 'moment'
+import ConfirmDialog from '../../common/dialog/ConfirmDialog'
+import FormState from '../../utils/formState'
+import { useSharedState } from '../../shared/sharedState'
+import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { createVaccineImmunization } from '../ClientDetailsView/DataWrapper'
-import { useNavigate } from "react-router-dom"
-import { useApiRequest } from "../../api/useApiRequest"
+import { useNavigate } from 'react-router-dom'
+import { useApiRequest } from '../../api/useApiRequest'
+import { DatePicker, Input, Select, Form } from 'antd'
 
 export default function NotAdministered() {
   const navigate = useNavigate()
   const { sharedData } = useSharedState()
+  const [form] = Form.useForm()
 
   const [isDialogOpen, setDialogOpen] = useState(false)
-  const [selectedVaccine, setSelectedVaccine] = useState(null)
-  const [vaccinesToNotAdminister, setVaccinesToNotAdminister] = useState([])
   const [vaccines, setVaccines] = useState([])
   const [vaccinesToSelect, setVaccinesToSelect] = useState([])
 
   useEffect(() => {
-    const vaccinesToSelect = sharedData.map((vaccine) => ({ name: vaccine.vaccineName, value: vaccine.vaccineName }))
+    const vaccinesToSelect = sharedData.map((vaccine) => ({
+      label: vaccine.vaccineName,
+      value: vaccine.vaccineName,
+    }))
+    if (vaccinesToSelect.length === 0) {
+      navigate(-1)
+    }
     setVaccinesToSelect(vaccinesToSelect)
     setVaccines(sharedData)
-
-    setVaccines(vaccinesToSelect)
   }, [sharedData])
 
   const currentPatient = useSelector((state) => state.currentPatient)
   const { post } = useApiRequest()
 
-  const { formData, formErrors, handleChange } = FormState({
-    vaccinesToContraindicate: '',
-    batchNumbers: [],
-    contraindicationDetails: '',
-    nextVaccinationDate: '',
-  }, {})
 
   const fhirReasons = [
-    { name: 'Immunity', value: 'IMMUNE' },
-    { name: 'Medical precaution', value: 'MEDPREC' },
+    { label: 'Immunity', value: 'IMMUNE' },
+    { label: 'Medical precaution', value: 'MEDPREC' },
     //{ name: 'Product out of stock', value: 'OSTOCK' },
     // { name: 'Patient objection', value: 'PATOBJ' },
-    { name: 'Caregiver refusal', value: 'PHILISOP' },
-    { name: 'Religious objection', value: 'RELIG' },
-    { name: 'Cold Chain Break', value: 'VACEFF' },
-    { name: 'Expired Product', value: 'VACSAF' },
+    { label: 'Caregiver refusal', value: 'PHILISOP' },
+    { label: 'Religious objection', value: 'RELIG' },
+    { label: 'Cold Chain Break', value: 'VACEFF' },
+    { label: 'Expired Product', value: 'VACSAF' },
   ]
 
   function handleDialogClose() {
@@ -53,123 +51,143 @@ export default function NotAdministered() {
     setDialogOpen(false)
   }
 
-  const handleFormSubmit = async () => {
-    if (Array.isArray(vaccines) && vaccines.length > 0) {
-      const data = vaccines.map((immunization) => {
-        immunization.contraindicationDetails = formData.contraindicationDetails
-        immunization.education = [
-          { presentationDate: formData.nextVaccinationDate }
-        ]
-        return createVaccineImmunization(
-          immunization,
-          currentPatient.id,
-          'not-done'
-        )
-      })
+  console.log('vaccines', vaccines)
 
-      const responses = await Promise.all(
-        data?.map(async (administerVaccine) => {
-          return await post('/hapi/fhir/Immunization', administerVaccine)
-        })
+  const handleFormSubmit = async (values) => {
+    console.log(vaccines)
+    const selectedVaccines = vaccines.filter((vaccine) =>
+      values.vaccinesToContraindicate.includes(vaccine.vaccineName)
+    )
+
+    const data = selectedVaccines.map((immunization) => {
+      immunization.contraindicationDetails = values.contraindicationDetails
+      immunization.education = [
+        {
+          presentationDate: values.nextVaccinationDate.format('YYYY-MM-DD'),
+        },
+      ]
+      return createVaccineImmunization(
+        immunization,
+        currentPatient.id,
+        'not-done'
       )
+    })
 
-      if (responses) {
-        setDialogOpen(true)
-        const time = setTimeout(() => {
-          setDialogOpen(false)
-          navigate(-1)
-        }, 2000)
-        return () => clearTimeout(time)
-      }
+    const responses = await Promise.all(
+      data?.map(async (administerVaccine) => {
+        return await post('/hapi/fhir/Immunization', administerVaccine)
+      })
+    )
+
+    if (responses) {
+      setDialogOpen(true)
+      const time = setTimeout(() => {
+        setDialogOpen(false)
+        navigate(-1)
+      }, 2000)
+      return () => clearTimeout(time)
     }
   }
 
   return (
     <>
-
       <ConfirmDialog
         open={isDialogOpen}
         description={`Vaccine not administered recorded`}
-        onClose={handleDialogClose} />
-  
+        onClose={handleDialogClose}
+      />
+
       <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow mt-5 full-width">
         <div className="px-4 text-2xl font-semibold py-5 sm:px-6">
           Not Administered
         </div>
         <div className="px-4 py-5 sm:p-6">
-          <div className="grid grid-cols-2 gap-36 px-6 gap-10">
-            <div>
-
-              <SelectMenu
-                data={vaccinesToSelect}
-                error={formErrors.identificationType}
-                required={true}
-                label="Vaccines not administered"
-                value={selectedVaccine || 'Vaccines not administered'}
-                onInputChange={(value) => {
-                  const vaccineExists = vaccines.find((vaccine) => vaccine.vaccineName === value.name)
-                  if (!vaccineExists) {
-                    const inShared = sharedData.find((vaccine) => vaccine.vaccineName === value.name)
-                    setVaccines([...vaccines, inShared ])
-                  }
-                }}/>
-
-              <div className="py-4 flex">
-                {vaccines.map((vaccine) => (
-                  <span class="inline-flex items-center gap-x-3 py-2 ps-5 pe-2 rounded-full text-xs font-medium bg-blue-100 mx-2">
-                    {vaccine.name}
-                    <button
-                      onClick={() => {
-                        const vaccinesLeft = vaccines.filter((vacc) => vacc.vaccineName !== vaccine.vaccineName)
-                        setVaccines(vaccinesLeft)
-                      }}
-                      type="button"
-                      class="flex-shrink-0 size-4 inline-flex items-center justify-center rounded-full hover:bg-blue-200 focus:outline-none focus:bg-blue-200">
-                      <span class="sr-only">Remove badge</span>
-                      <svg class="flex-shrink-0 size-3" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
-                    </button>
-                  </span>
-                ))}
-                
+          {vaccines.length > 0 && (
+            <Form
+              layout="vertical"
+              onFinish={handleFormSubmit}
+              form={form}
+              initialValues={{
+                vaccinesToContraindicate: vaccinesToSelect.map(
+                  (vaccine) => vaccine.value
+                ),
+              }}
+              className="grid grid-cols-2 gap-36 px-6 gap-10"
+            >
+              <div>
+                <Form.Item
+                  label="Vaccines not administered"
+                  name="vaccinesToContraindicate"
+                  rules={[
+                    { required: true, message: 'Please select a vaccine' },
+                  ]}
+                >
+                  <Select
+                    mode="multiple"
+                    placeholder="Select a vaccine"
+                    options={vaccinesToSelect}
+                    showSearch
+                    size="large"
+                  />
+                </Form.Item>
               </div>
 
-            </div>
+              <div>
+                <Form.Item
+                  label="Reason for not being vaccinated"
+                  name="notVaccinatedReason"
+                  rules={[
+                    { required: true, message: 'Please select a reason' },
+                  ]}
+                >
+                  <Select
+                    placeholder="Select a Reason"
+                    options={fhirReasons}
+                    showSearch
+                    size="large"
+                  />
+                </Form.Item>
 
-            <div>
-              <SelectMenu
-                data={fhirReasons}
-                error={formErrors.notVaccinatedReason}
-                required={true}
-                label="Reasons for not vaccinated"
-                value={formData.notVaccinatedReason || 'Select Reason'}
-                onInputChange={(value) => handleChange('vaccinesToContraindicate', value.name)}/>
-
-                <TextInput
-                  inputType="date"
-                  inputName="nextVaccinationDate"
-                  inputId="nextVaccinationDate"
-                  required={true}
-                  label="Next vaccination date"
-                  value={formData.nextVaccinationDate}
-                  onInputChange={(value) => handleChange('nextVaccinationDate', value)}/>
-            </div>
-          </div>
+                <Form.Item
+                  label="Next Vaccination Date"
+                  name="nextVaccinationDate"
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Please enter next vaccination date',
+                    },
+                  ]}
+                >
+                  <DatePicker
+                    size="large"
+                    style={{ width: '100%' }}
+                    format="DD-MM-YYYY"
+                    disabledDate={(current) =>
+                      current &&
+                      (current < moment().startOf('day') ||
+                        current > moment().add(14, 'days').startOf('day'))
+                    }
+                  />
+                </Form.Item>
+              </div>
+            </Form>
+          )}
         </div>
         <div className="px-4 py-4 sm:px-6 flex justify-end">
           <button
             onClick={() => navigate(-1)}
-            className="ml-4 flex-shrink-0 rounded-md outline outline-[#163C94] px-10 py-2 text-sm font-semibold text-[#163C94] shadow-sm hover:bg-[#163C94] hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            className="ml-4 flex-shrink-0 rounded-md outline outline-[#163C94] px-10 py-2 text-sm font-semibold text-[#163C94] shadow-sm hover:bg-[#163C94] hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
             Cancel
           </button>
           <button
             onClick={() => {
-              setDialogOpen(true)
-              handleFormSubmit()
+              form.submit()
             }}
-            className="ml-4 flex-shrink-0 rounded-md outline bg-[#4e8d6e] outline-[#4e8d6e] px-10 py-2 text-sm font-semibold text-white shadow-sm hover:bg-[#4e8d6e] hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            className="ml-4 flex-shrink-0 rounded-md outline bg-[#4e8d6e] outline-[#4e8d6e] px-10 py-2 text-sm font-semibold text-white shadow-sm hover:bg-[#4e8d6e] hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
             Submit
           </button>
-          
         </div>
       </div>
     </>
