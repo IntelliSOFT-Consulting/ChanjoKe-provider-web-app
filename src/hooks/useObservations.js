13c30374cc6920e4731fd32910686fb00b66cb3f
nyatindopatrick <nyatindopatrick@gmail.com>
2024-04-15 14:13:20 +0300

    update weight on administer

--- src/hooks/useObservations.js
+++ src/hooks/useObservations.js
@@ -1,17 +1,19 @@
 import { useState } from 'react'
 import { useApiRequest } from '../api/useApiRequest'
 
+const fhirApi = '/hapi/fhir'
 export default function useObservations() {
   const { get } = useApiRequest()
   const [observations, setObservations] = useState([])
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(null)
 
+
   const getObservations = async (patientId) => {
     setLoading(true)
     try {
-      const response = await get(`Observation?patient=Patient/${patientId}`)
-      setObservations(response.data.entry)
+      const response = await get(`${fhirApi}/Observation?patient=Patient/${patientId}`)
+      setObservations(response.entry)
       setLoading(false)
     } catch (error) {
       setError(error)
@@ -21,8 +23,8 @@ export default function useObservations() {
 
   const getLatestObservation = async (patientId) => {
     const today = new Date().toISOString().split('T')[0]
-    const response = await get(`Observation?patient=Patient/${patientId}&_sort=-_lastUpdated&_lastUpdated=${today}`)
-    return response.data.entry[0];
+    const response = await get(`${fhirApi}/Observation?patient=Patient/${patientId}&_sort=-_lastUpdated&_lastUpdated=${today}`)
+    return response.entry[0];
   }
 
   return { observations, loading, error, getObservations, getLatestObservation }
