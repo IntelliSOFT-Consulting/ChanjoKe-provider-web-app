122355cae99ff3db14ff5aa67fd5935956d35627
Onejohi <tonnie254@live.com>
2024-04-16 07:55:58 +0300

    - monday fixes

--- src/components/RegisterClient/convertObjectToArray.js
+++ src/components/RegisterClient/convertObjectToArray.js
@@ -8,12 +8,13 @@ dayjs.extend(localeDate)
 function convertCamelCaseString(inputString) {
   let stringWithSpaces = inputString.replace(/([a-z])([A-Z])/g, '$1 $2');
   stringWithSpaces = stringWithSpaces.replace(/\b\w+/g, (word) => {
-    // Check if the word includes 'of', and if so, keep it lowercase
-    if (word.toLowerCase().includes('of') || word.toLowerCase().includes('or')) {
+    const lowercaseWords = ['of', 'or'];
+    if (lowercaseWords.includes(word.toLowerCase())) {
       return word.toLowerCase();
     }
     return word.charAt(0).toUpperCase() + word.slice(1);
   });
+  
   return stringWithSpaces;
 }
 
@@ -24,8 +25,10 @@ const ConvertObjectToArray = (inputObject) => {
   for (const key in inputObject) {
     const item = {
       title: `${convertCamelCaseString(key)}`,
-      value: typeof inputObject[key] === 'object' ? dayjs(inputObject[key]).format('DD-MM-YYYY') :  inputObject[key],
+      value: typeof inputObject[key] === 'object' ? dayjs(inputObject[key]).format('DD-MM-YYYY') :  convertCamelCaseString(inputObject[key]),
     };
+
+    console.log({ item })
 
     clientDetails.push(item);
   }
