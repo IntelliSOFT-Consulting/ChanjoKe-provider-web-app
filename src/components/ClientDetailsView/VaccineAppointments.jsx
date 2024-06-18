75c8e919e66e4c9285e78c95281c9d581b2e1ff3
Samuel Muli <samuelmuli95@gmail.com>
2024-06-18 11:54:12 +0300

    Fix new appointment button

--- src/components/ClientDetailsView/VaccineAppointments.jsx
+++ src/components/ClientDetailsView/VaccineAppointments.jsx
@@ -154,5 +154,3 @@ export default function VaccineAppointments({ userCategory, patientData, patient
     </div>
   )
 }
-
-// flex-shrink-0 place-self-end rounded-md bg-[#163C94] border border-[#163C94] outline outline-[#163C94] px-5 py-2 text-sm font-semibold text-white shadow-sm hover:bg-[#163C94] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#163C94]
