970628d3eb57f3188354f3e71729433569fb31a6
nyatindopatrick <nyatindopatrick@gmail.com>
2024-09-09 12:32:02 +0300

    Fix user update

--- src/components/AdminManagement/AddUser.jsx
+++ src/components/AdminManagement/AddUser.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react'
-import { Button, Form, Input, InputNumber, Select, notification } from 'antd'
 import { useSelector } from 'react-redux'
+import { Button, Form, Input, InputNumber, Select, notification } from 'antd'
 import LoadingArrows from '../../common/spinners/LoadingArrows'
 import { roleGroups } from '../../data/options/clientDetails'
 import { useLocations } from '../../hooks/useLocation'
 import { usePractitioner } from '../../hooks/usePractitioner'
+import { hyphenToCamel } from '../../utils/formatter'
 import { titleCase } from '../../utils/methods'
-import { camelToHyphen, hyphenToCamel } from '../../utils/formatter'
 
 const AddUser = ({
   setVisible,
@@ -19,17 +19,17 @@ const AddUser = ({
   const [form] = Form.useForm()
   const { user } = useSelector((state) => state.userInfo)
   const [role, setRole] = useState(null)
-  const [locationOptions, setLocationOptions] = useState({
-    country: [{ value: '0', label: 'Kenya' }],
-    county: [],
-    subCounty: [],
-    ward: [],
-    facility: [],
-  })
-
   const [api, contextHolder] = notification.useNotification()
 
-  const { fetchLocations } = useLocations(form)
+  const {
+    handleCountyChange,
+    handleSubCountyChange,
+    handleWardChange,
+    counties,
+    subCounties,
+    wards,
+    facilities,
+  } = useLocations(form)
 
   const {
     handleCreatePractitioner,
@@ -40,25 +40,28 @@ const AddUser = ({
 
   useEffect(() => {
     if (visible?.id) {
-      fetchPractitioner(visible.id)
-    }
-    if (!visible) {
+      fetchPractitionerDetails(visible.id)
+    } else {
       resetForm()
     }
   }, [visible])
 
-  const fetchPractitioner = async (id) => {
-    const practitionerData = await getPractitioner(id)
-
-    form.setFieldsValue({
-      ...practitionerData,
-      phoneNumber: practitionerData?.phone,
-      roleGroup: practitionerData?.practitionerRole,
-    })
-    const selected = roleGroups?.find(
-      (role) => role.value === practitionerData?.practitionerRole
-    )
-    setRole(selected)
+  const fetchPractitionerDetails = async (id) => {
+    try {
+      const practitionerData = await getPractitioner(id)
+      await loadLocationHierarchy(practitionerData)
+      form.setFieldsValue({
+        ...practitionerData,
+        phoneNumber: practitionerData?.phone,
+        roleGroup: practitionerData?.practitionerRole,
+      })
+      const selectedRole = roleGroups.find(
+        (role) => role.value === practitionerData?.practitionerRole
+      )
+      setRole(selectedRole)
+    } catch (error) {
+      console.error('Error fetching practitioner details:', error)
+    }
   }
 
   const handleFinish = async (values) => {
@@ -71,7 +74,7 @@ const AddUser = ({
       resetForm()
       await fetchPractitioners(null, activeTab === '1', currentPage)
     } catch (error) {
-      console.log('error', error)
+      console.error('Error handling form submission:', error)
       api.error({
         message: 'Error',
         description: error?.response?.data?.message || 'An error occurred',
@@ -83,73 +86,52 @@ const AddUser = ({
     form.resetFields()
     setPractitionerData(null)
     setRole(null)
-    setLocationOptions({
-      country: [{ value: '0', label: 'Kenya' }],
-      county: [],
-      subCounty: [],
-      ward: [],
-      facility: [],
-    })
     setVisible(false)
   }
 
   const loadLocationHierarchy = async (userData) => {
-    const hierarchy = ['country', 'county', 'subCounty', 'ward', 'facility']
-    let parentId = '0'
-    let options = { ...locationOptions }
-
-    for (const level of hierarchy) {
-      if (userData[level]) {
-        const locations = await fetchLocations(parentId, level.toUpperCase())
-        options[level] = locations?.map((loc) => ({
-          value: loc.key,
-          label: loc.name,
-        }))
-        parentId = userData[level]
-      } else {
-        break
-      }
-    }
-
-    setLocationOptions(options)
+    const { county, subCounty, ward } = userData
+    if (county) await handleCountyChange(county)
+    if (subCounty) await handleSubCountyChange(subCounty)
+    if (ward) await handleWardChange(ward)
   }
 
   const handleLocationChange = async (value, level) => {
-    const hierarchy = ['country', 'county', 'subCounty', 'ward', 'facility']
-    const currentIndex = hierarchy.indexOf(level)
-    if (currentIndex === hierarchy.length - 1) return
-
-    const nextLevel = hierarchy[currentIndex + 1]
-
-    const locations = await fetchLocations(
-      value,
-      camelToHyphen(nextLevel).toUpperCase()
-    )
-    const newOptions = locations?.map((loc) => ({
-      value: loc.key,
-      label: loc.name,
-    }))
-
-    setLocationOptions((prevOptions) => ({
-      ...prevOptions,
-      [nextLevel]: newOptions,
-      ...hierarchy
-        .slice(currentIndex + 2)
-        .reduce((acc, level) => ({ ...acc, [level]: [] }), {}),
-    }))
-
-    form.setFieldsValue({
-      ...hierarchy
-        .slice(currentIndex + 1)
-        .reduce((acc, level) => ({ ...acc, [level]: undefined }), {}),
-    })
+    await loadLocationHierarchy({ [level]: value })
   }
 
-  const renderPersonalDetails = () => (
+  const getOptions = (field) => {
+    const optionsMap = {
+      country: [{ value: '0', label: 'Kenya' }],
+      county: counties?.map((county) => ({
+        value: county.key,
+        label: county.name,
+      })),
+      subCounty: subCounties?.map((subCounty) => ({
+        value: subCounty.key,
+        label: subCounty.name,
+      })),
+      ward: wards?.map((ward) => ({ value: ward.key, label: ward.name })),
+      facility: facilities?.map((facility) => ({
+        value: facility.key,
+        label: facility.name,
+      })),
+    }
+    return optionsMap[field] || []
+  }
+
+  const renderSection = (title, children) => (
     <>
       <h4 className="text-primary border-b-2 border-primary w-full font-semibold text-base mb-6">
-        Personal Details
+        {title}
       </h4>
+      {children}
+    </>
+  )
+
+  const renderPersonalDetails = () =>
+    renderSection(
+      'Personal Details',
       <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
         <Form.Item
           label="First Name"
@@ -174,7 +156,7 @@ const AddUser = ({
             { required: true, message: 'ID Number is required' },
             {
               validator: async (_, value) => {
-                if (value && value?.toString().length >= 6 && !visible?.id) {
+                if (value && value.toString().length >= 6 && !visible?.id) {
                   const response = await searchPractitioner({
                     identifier: value,
                   })
@@ -190,14 +172,11 @@ const AddUser = ({
           <InputNumber className="w-full" placeholder="ID Number" />
         </Form.Item>
       </div>
-    </>
-  )
+    )
 
-  const renderContactDetails = () => (
-    <>
-      <h4 className="text-primary border-b-2 border-primary w-full font-semibold text-base mb-6">
-        Contact Details
-      </h4>
+  const renderContactDetails = () =>
+    renderSection(
+      'Contact Details',
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
         <Form.Item
           label="Phone Number"
@@ -213,9 +192,7 @@ const AddUser = ({
             {
               validator: async (_, value) => {
                 if (value && value.length === 10 && !visible?.id) {
-                  const response = await searchPractitioner({
-                    telecom: value,
-                  })
+                  const response = await searchPractitioner({ telecom: value })
                   if (response?.length) {
                     return Promise.reject(
                       new Error('Phone number already exists')
@@ -258,14 +235,11 @@ const AddUser = ({
           <Input placeholder="Email" />
         </Form.Item>
       </div>
-    </>
-  )
+    )
 
-  const renderRoleDetails = () => (
-    <>
-      <h4 className="text-primary border-b-2 border-primary w-full font-semibold text-base mb-6">
-        Role Details
-      </h4>
+  const renderRoleDetails = () =>
+    renderSection(
+      'Role Details',
       <div className="grid grid-cols-1 md:grid-cols-3 gap-10 mb-4">
         <Form.Item
           label="Select Role"
@@ -276,10 +250,10 @@ const AddUser = ({
             placeholder="Select Role"
             showSearch
             filterOption={(input, option) =>
-              option?.label.toLowerCase()?.includes(input?.toLowerCase())
+              option?.label.toLowerCase().includes(input.toLowerCase())
             }
             onChange={async (value) => {
-              const selected = roleGroups?.find((role) => role.value === value)
+              const selected = roleGroups.find((role) => role.value === value)
               setRole(selected)
               form.setFieldsValue({
                 country: '0',
@@ -288,16 +262,14 @@ const AddUser = ({
                 ward: undefined,
                 facility: undefined,
               })
-              await handleLocationChange('0', 'country')
             }}
-            options={roleGroups?.filter((role) =>
+            options={roleGroups.filter((role) =>
               role.creators.includes(user?.practitionerRole)
             )}
           />
         </Form.Item>
       </div>
-    </>
-  )
+    )
 
   const renderLocationDetails = () => {
     if (!role) return null
@@ -312,42 +284,34 @@ const AddUser = ({
     const roleLocationType = role.locations[0]
     const roleLocationIndex = locationTypes.indexOf(roleLocationType)
 
-    return (
-      <>
-        <h4 className="text-primary border-b-2 border-primary w-full font-semibold text-base mb-6">
-          Location Details
-        </h4>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-x-10">
-          {locationTypes
-            .slice(0, roleLocationIndex + 1)
-            ?.map((locationType) => {
-              const fieldName = hyphenToCamel(locationType.toLowerCase())
-              return (
-                <Form.Item
-                  key={fieldName}
-                  label={`Select ${titleCase(locationType)}`}
-                  name={fieldName}
-                  rules={[
-                    { required: true, message: `Please select a ${fieldName}` },
-                  ]}
-                >
-                  <Select
-                    placeholder={`Select ${locationType}`}
-                    options={locationOptions[fieldName]}
-                    showSearch
-                    allowClear
-                    filterOption={(input, option) =>
-                      option?.label
-                        .toLowerCase()
-                        ?.includes(input?.toLowerCase())
-                    }
-                    onChange={(value) => handleLocationChange(value, fieldName)}
-                  />
-                </Form.Item>
-              )
-            })}
-        </div>
-      </>
+    return renderSection(
+      'Location Details',
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-x-10">
+        {locationTypes.slice(0, roleLocationIndex + 1).map((locationType) => {
+          const fieldName = hyphenToCamel(locationType.toLowerCase())
+          return (
+            <Form.Item
+              key={fieldName}
+              label={`Select ${titleCase(locationType)}`}
+              name={fieldName}
+              rules={[
+                { required: true, message: `Please select a ${fieldName}` },
+              ]}
+            >
+              <Select
+                placeholder={`Select ${titleCase(locationType)}`}
+                options={getOptions(fieldName)}
+                showSearch
+                allowClear
+                filterOption={(input, option) =>
+                  option?.label.toLowerCase().includes(input.toLowerCase())
+                }
+                onChange={(value) => handleLocationChange(value, fieldName)}
+              />
+            </Form.Item>
+          )
+        })}
+      </div>
     )
   }
 
