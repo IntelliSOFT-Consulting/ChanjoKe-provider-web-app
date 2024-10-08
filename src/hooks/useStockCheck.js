e7fa946ace8b8506d3671740b640277be1393c63
nyatindopatrick <nyatindopatrick@gmail.com>
2024-10-08 09:18:29 +0300

    Memoize stockCheck calculation

--- src/hooks/useStockCheck.js
+++ src/hooks/useStockCheck.js
@@ -1,58 +1,49 @@
-// for each vaccine, check if the stock is less than the minimum stock
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import useInventory from './useInventory'
-import { useVaccineLevels } from './useVaccineLevels'
-import { Alert } from 'antd'
+import { useEffect, useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import useInventory from './useInventory';
+import { useVaccineLevels } from './useVaccineLevels';
 
 export const useStockCheck = () => {
-  const dispatch = useDispatch()
-  const { getAggregateInventoryItems, inventoryItems } = useInventory()
-  const { fetchVaccineLevels } = useVaccineLevels()
-  const { vaccineLevels } = useSelector((state) => state.vaccineSchedules)
-  const { user } = useSelector((state) => state.userInfo)
+  const { getAggregateInventoryItems, inventoryItems } = useInventory();
+  const { fetchVaccineLevels } = useVaccineLevels();
+  const { vaccineLevels } = useSelector((state) => state.vaccineSchedules);
+  const { user } = useSelector((state) => state.userInfo);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      fetchVaccineLevels()
-    }, 10000)
-    return () => clearInterval(interval)
-  }, [])
+    const interval = setInterval(fetchVaccineLevels, 10000);
+    return () => clearInterval(interval);
+  }, []);
 
   useEffect(() => {
     if (user?.orgUnit?.code && vaccineLevels?.id) {
-      getAggregateInventoryItems()
+      getAggregateInventoryItems();
     }
-  }, [vaccineLevels])
+  }, [user?.orgUnit?.code, vaccineLevels]);
 
-  const stockCheck = () => {
-    const parameter = vaccineLevels?.parameter
-    const stockCheck = parameter?.map((item) => {
-      const { name, min, max } = item
-      const stock =
-        inventoryItems?.find((item) => item.vaccine === name)?.quantity ?? 0
+  const stockCheck = useMemo(() => {
+    return vaccineLevels?.parameter?.map(({ name, min, max }) => {
+      const stock = inventoryItems?.find((item) => item.vaccine === name)?.quantity || 0;
       return {
         vaccine: name,
         minimum: min,
         maximum: max,
         stock,
-      }
-    })
+      };
+    }) || [];
+  }, [vaccineLevels, inventoryItems]);
 
-    return stockCheck
-  }
+  const belowMinimumStock = useMemo(() => 
+    stockCheck.filter(({ stock, minimum }) => stock && minimum && stock < minimum).map(({ vaccine }) => vaccine).join(', '), 
+    [stockCheck]
+  );
 
-  const belowMinimumStock = stockCheck()
-    ?.filter((item) => item.stock && item.minimum && item.stock < item.minimum)
-    ?.map((item) => item.vaccine)
-    ?.join(', ')
-  const aboveMaximumStock = stockCheck()
-    ?.filter((item) => item.stock && item.maximum && item.stock > item.maximum)
-    ?.map((item) => item.vaccine)
-    ?.join(', ')
+  const aboveMaximumStock = useMemo(() => 
+    stockCheck.filter(({ stock, maximum }) => stock && maximum && stock > maximum).map(({ vaccine }) => vaccine).join(', '), 
+    [stockCheck]
+  );
 
   return {
     belowMinimumStock,
     aboveMaximumStock,
-  }
-}
+  };
+};
